75a5d55492501f0407f120bb9a09c8886f993009
iStratix <itsnagle@gmail.com>
2023-02-24 15:54:41 -0500

    Add project files.

---
+++ DequeInstructions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DequeInstructions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
