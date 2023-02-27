0b290a5951dae910cf7ac7bbe3d854e95afb735e
iStratix <itsnagle@gmail.com>
2023-02-27 12:25:39 -0500

    CurrentLocation added

--- DequeInstructions/MyDeque.cs
+++ DequeInstructions/MyDeque.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// Author: Travis Nagle, Naglet@etsu.edu
+// Course: CSCI-2210-001 - Data Structures
+// Assignment: Project 3 - DequeInstructions
+// Description: Attempted implementation of the Deque data structure
+//
+///////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -7,10 +17,11 @@ using System.Threading.Tasks;
 
 namespace DequeInstructions
 {
-    public class MyDeque<T> : IStack<T>, IQueue<T>
+    public class MyDeque<T> : IStack<T>, IQueue<T>, ICollection
     {
         private T[] MyArray { get; set; }
         private int _count;
+        private int _currentLocation;
 
         public MyDeque(int maxSize = 10)
         {
@@ -29,15 +40,52 @@ namespace DequeInstructions
             }
         }
 
-        public T Dequeue()
+        public int CurrentLocation
         {
-
-            throw new NotImplementedException();
+            get
+            {
+                return _currentLocation;
+            }
+            set
+            {
+                _currentLocation = Math.Min(MyArray.Length - 1, value);
+            }
         }
 
+        public bool IsSynchronized => false;
+
+        public object SyncRoot => this;
+
+        public T Dequeue() //Pulling from the start of the line
+        {
+            T initialResult = MyArray[0];
+            if(Count == 0)
+            {
+                return default(T);
+            }
+            for(int i = Count - 1; i > 0; i--)
+            {
+                MyArray[i - 1] = MyArray[i];
+            }
+            Count--;
+            return initialResult;
+        }
+        
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            if(Count != MyArray.Length)
+            {
+                for(int i = 1; i < MyArray.Length; i++)
+                {
+                    MyArray[i] = MyArray[i - 1];
+                }
+                MyArray[0] = item;
+                Count++;
+            }
+            else
+            {
+                throw new Exception("The Deque is full");
+            }
         }
 
         public T Peek()
@@ -63,6 +111,26 @@ namespace DequeInstructions
         {
             MyArray[Count] = item;
             Count++;
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            string info = "";
+            for(int i  = 0; i < MyArray.Length; i++)
+            {
+                info += $"{MyArray[i]}\n";
+            }
+            return info;
         }
     }
 }
--- DequeInstructions/Program.cs
+++ DequeInstructions/Program.cs
@@ -7,9 +7,16 @@
             MyDeque<int> deque= new MyDeque<int>();
             deque.Push(15);
             deque.Push(15);
+            deque.Enqueue(12);
             deque.Push(15);
             deque.Push(15);
+            deque.Enqueue(12);
+            deque.Enqueue(12);
             deque.Push(15);
+            deque.Push(15);
+            //deque.Dequeue();
+            Console.WriteLine(deque);
+
         }
     }
 }
