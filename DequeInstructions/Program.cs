8588c6f9d6f9b298804d7aefcb47af56c66693e3
Travis Nagle <NAGLET@ETSU.EDU>
2023-02-28 18:30:17 -0500

    Complete program layout

--- DequeInstructions/Program.cs
+++ DequeInstructions/Program.cs
@@ -7,12 +7,21 @@
             MyDeque<int> deque= new MyDeque<int>(6);
             deque.Enqueue(1);
             deque.Push(15);
-            Console.WriteLine(deque.Pop());
-            Console.WriteLine(deque.Pop());
-            Console.WriteLine(deque.Dequeue());
-            Console.WriteLine(deque.Dequeue());
+            deque.Enqueue(12);
+            deque.Push(13);
 
+            Console.WriteLine("----Internal Array----");
             Console.WriteLine(deque);
+            Console.WriteLine();
+
+            Console.WriteLine("----Peek----");
+            Console.WriteLine(deque.Peek());
+            Console.WriteLine("----Pop----");
+            Console.WriteLine(deque.Pop());
+            Console.WriteLine(deque.Pop());
+            Console.WriteLine("----Dequeue----");
+            Console.WriteLine(deque.Dequeue());
+            Console.WriteLine(deque.Dequeue());
         }
     }
 }
