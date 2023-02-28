ed120e98b99341b2c58646702f8a77cd25131bec
Travis Nagle <NAGLET@ETSU.EDU>
2023-02-28 17:23:24 -0500

    added XML to stack functions

--- DequeInstructions/MyDeque.cs
+++ DequeInstructions/MyDeque.cs
@@ -18,6 +18,10 @@ using System.Threading.Tasks;
 
 namespace DequeInstructions
 {
+    /// <summary>
+    /// Implementation of the MyDeque class which allows for items to be pulled from the front and back
+    /// </summary>
+    /// <typeparam name="T">Generic data type</typeparam>
     public class MyDeque<T> : IStack<T>, IQueue<T>
     {
         private T[] MyArray { get; set; }
@@ -26,12 +30,19 @@ namespace DequeInstructions
         private int _queueCount;
         private int totalCount = 0;
 
+        /// <summary>
+        /// Default constructor for the MyDeque object which sets up the size of the internal array
+        /// </summary>
+        /// <param name="maxSize">Size of the array</param>
         public MyDeque(int maxSize = 10)
         {
             MyArray = new T[maxSize];
             CurrentLocation = MyArray.Length - 1;
         }
 
+        /// <summary>
+        /// Keeps track of the location within the array with stack functions
+        /// </summary>
         public int StackCount
         {
             get 
@@ -82,6 +93,10 @@ namespace DequeInstructions
             CurrentLocation--;
         }
 
+        /// <summary>
+        /// Looks at the next item in the array without incrementing the counters
+        /// </summary>
+        /// <returns>The next value in line</returns>
         public T Peek()
         {
             if (StackCount == 0)
@@ -91,6 +106,10 @@ namespace DequeInstructions
             return MyArray[StackCount - 1];
         }
 
+        /// <summary>
+        /// Pop method which pulls a value from the front of the array
+        /// </summary>
+        /// <returns>Value in front of line</returns>
         public T Pop()
         {
             if(StackCount == 0)
@@ -103,6 +122,10 @@ namespace DequeInstructions
             return result;
         }
 
+        /// <summary>
+        /// Push method that places a new value in the front of the array
+        /// </summary>
+        /// <param name="item">Item to be placed in line</param>
         public void Push(T item)
         {
             MyArray[StackCount] = item;
